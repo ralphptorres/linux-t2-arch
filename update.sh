6641b498f2b6bd28e1af9ab3eece9c939dad9bf5
Redecorating <orlandoch.dev@gmail.com>
2023-01-23 15:18:34 +1100

    Autoupdate (#24)
    Based off of work in https://github.com/t2linux/T2-Ubuntu-Kernel

--- update.sh
+++ update.sh
@@ -8,21 +8,22 @@ ARCH_VER=$(curl -s https://archlinux.org/packages/core/x86_64/linux/ | \
 	tr " " $'\n' | grep arch | cut -d- -f1)
 
 VER=$(echo $ARCH_VER | rev | cut -d. -f2- | rev)
+OLD_VER=$(grep pkgver= PKGBUILD | cut -d= -f2-)
+
+if [ $OLD_VER == $VER ]; then
+	echo "Up to date ($VER)."
+	exit 0
+fi
 
 T2_PATCH_HASH=$(git ls-remote https://github.com/t2linux/linux-t2-patches.git refs/heads/main | cut -d$'\t' -f1)
 
 curl -s https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/linux/repos/core-x86_64/PKGBUILD > PKGBUILD.orig
 curl -s https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/linux/repos/core-x86_64/config > config
 
-sed -i s/T2_PATCH_HASH=.*/T2_PATCHE_HASH=$T2_PATCH_HASH/ PKGBUILD
+sed -i s/T2_PATCH_HASH=.*/T2_PATCH_HASH=$T2_PATCH_HASH/ PKGBUILD
 sed -i s/pkgrel=./pkgrel=1/ PKGBUILD
 sed -i s/pkgver=.*/pkgver=$VER/ PKGBUILD
 
 updpkgsums
 
-vimdiff PKGBUILD PKGBUILD.orig
-
-makepkg -Cso
-
-
-git diff
+touch do_pr
